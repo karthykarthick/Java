c5bc70942055214b75bccc3a4328e48aa7094113
conne <conne@host.docker.internal>
2021-10-07 18:43:10 +0530

    test commit

---
+++ src/testcom/test/test.java
@@ -0,0 +1,15 @@
+package testcom.test;
+
+public class test {
+
+	public test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from chennai!");
+
+	}
+
+}
